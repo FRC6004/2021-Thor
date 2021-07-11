712b0f8cdd42b2fe97c81152add7ffc33b47a725
DJB755 <Rlpbbro05@gmail.com>
2021-07-11 04:00:39 -0400

    Initial Commit of 2021 version of 2020 code.  Needs Done: elevator both sides independent, pivot brake mode, autonomous.

---
+++ src/main/java/frc/robot/Subsystems/Shooter.java
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+
+package frc.robot.Subsystems;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DriverStation;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import frc.robot.RobotMap;
+import frc.robot.Commands.*;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.VictorSP;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+
+
+/**
+ * An example subsystem. You can replace me with your own Subsystem.
+ */
+public class Shooter extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    WPI_VictorSPX VictorSPX = new WPI_VictorSPX(RobotMap.SHOOTER);
+    
+
+    public Shooter() {
+        //VictorSPX.setNeutralMode(NeutralMode.Brake);
+    }
+
+    @Override
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        setDefaultCommand(new ShooterCmd());
+    }
+  
+  
+      public void update(double motorSpeed) {
+        // Update motor speed to passed in value
+        //System.out.print(motorSpeed);
+        VictorSPX.setNeutralMode(NeutralMode.Brake);
+        VictorSPX.set(motorSpeed);
+        //System.out.print();
+      }
+      
+      
+  }
+
